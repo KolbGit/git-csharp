a49ca51e8ea82de5f34b4a727a4b5bb46a0b990c
Eugen <zenyakolb@gmail.com>
2023-08-06 18:52:40 +0300

    first commit today : Hello world

--- SimpleCSharpConsoleApp/Program.cs
+++ SimpleCSharpConsoleApp/Program.cs
@@ -6,16 +6,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            // Настройка консольного пользовательского интерфейса.
-            Console.Title = "My Rocking App";
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.BackgroundColor = ConsoleColor.Blue;
-            Console.WriteLine("*************************************");
-            Console.WriteLine("***** Welcome to My Rocking App *****");
-            Console.WriteLine("*******************★*****************");
-            Console.BackgroundColor = ConsoleColor.Black;
-            // Ожидание нажатия клавиши <Enter>.
-            Console.ReadLine();
+            Console.WriteLine("Hello world");
         }
     }
 }
