431e9a3cdbfb32d16f1bc6e2311fb6a756fde5cb
Eugen <zenyakolb@gmail.com>
2023-08-08 09:50:11 +0300

    lesson about var

--- SimpleCSharpConsoleApp/Program.cs
+++ SimpleCSharpConsoleApp/Program.cs
@@ -3,68 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace TypeConversions
+namespace ImplicitlyTypedLocalVars
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Gandi();
+            Console.WriteLine("***** Fun with Implicit Typing *****");
+            DeclareImplicitVars();
+            QueryOverInts();
+            Console.WriteLine();
+            Console.ReadKey();
         }
 
-        //static int Add(int x, int y)
-        //{ return x + y; }
-
-        //#region Narrowing / Widening examples
-        //static void NarrowingAttempt()
-        //{
-        //    byte myByte = 0;
-        //    int myInt = 200;
-
-        //    // Explicitly cast the int into a byte (no loss of data).
-        //    myByte = (byte)myInt;
-        //    Console.WriteLine("Value of myByte: {0}", myByte);
-        //}
-
-        //static void ProcessBytes()
-        //{
-        //    byte b1 = 100;
-        //    byte b2 = 250;
-
-        //    // This time, tell the compiler to add CIL code
-        //    // to throw an exception if overflow/underflow
-        //    // takes place.
-        //    try
-        //    {
-        //        byte sum = checked((byte)Add(b1, b2));
-        //        Console.WriteLine("sum = {0}", sum);
-        //    }
-        //    catch (OverflowException ex)
-        //    {
-        //        Console.WriteLine(ex.Message);
-        //    }
-        //}
-
-        //static void NarrowWithConvert()
-        //{
-        //    byte myByte = 0;
-        //    int myInt = 200;
-        //    myByte = Convert.ToByte(myInt);
-        //    Console.WriteLine("Value of myByte: {0}", myByte);
-        //}
-        //#endregion
-
-        #region Gandi
-        static void Gandi()
+        #region Implicit data typing
+        static void DeclareImplicitVars()
         {
-            byte aggression = 1;
+            // Implicitly typed local variables.
+            var myInt = 0;
+            var myBool = true;
+            var myString = "Time, marches on...";
 
-            byte democracyModifier = 2;
+            // Print out the underlying type.
+            Console.WriteLine("myInt is a: {0}", myInt.GetType().Name);
+            Console.WriteLine("myBool is a: {0}", myBool.GetType().Name);
+            Console.WriteLine("myString is a: {0}", myString.GetType().Name);
+        }
 
-            aggression = (byte)(aggression - democracyModifier);
+        static int GetAnInt()
+        {
+            var retVal = 9;
+            return retVal;
+        }
 
-            Console.WriteLine(aggression);
+        static void ImplicitTypingIsStrongTyping()
+        {
+            // The compiler knows "s" is a System.String.
+            var s = "This variable can only hold string data!";
+            s = "This is fine...";
+
+            // Can invoke any member of the underlying type.
+            string upper = s.ToUpper();
+
+            // Error! Can't assign numerical data to a a string!
+            // s = 44;
         }
         #endregion
+
+        #region LINQ example
+        static void QueryOverInts()
+        {
+            int[] numbers = { 10, 20, 30, 40, 1, 2, 3, 8 };
+            var subset = from i in numbers where i < 10 select i;
+
+            Console.Write("Values in subset: ");
+            foreach (var i in subset)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+
+            // Hmm...what type is subset?
+            Console.WriteLine("subset is a: {0}", subset.GetType().Name);
+            Console.WriteLine("subset is defined in: {0}", subset.GetType().Namespace);
+        }
+        #endregion
+
     }
+
+    #region Bad use of var!
+    // Uncomment to see compile errors.
+    //class ThisWillNeverCompile
+    //{
+    //    // Error! var cannot be used as field data!
+    //    private var myInt = 10;
+
+    //    // Error! var cannot be used as a return value
+    //    // or parameter type!
+    //    public var MyMethod(var x, var y) { }
+    //}
+    #endregion
 }
